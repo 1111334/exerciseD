535e808a1d5801f4851338205533fe98440d2f70
Mike <109431533+1111334@users.noreply.github.com>
2023-07-12 09:44:25 +0200

    Add files via upload

---
+++ src/co/develhope/introdution/Start.java
@@ -0,0 +1,7 @@
+package co.develhope.introdution;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello develhope java student 14/12/1998");
+    }
+}
